552e1611cac3425b441b3ff27026c19387b1eac9
nathanhall762 <4943@holbertonstudents.com>
2022-11-22 11:35:45 -0600

    added api code

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,9 +8,6 @@ from api.v1.view import app_views
 app = Flask(__name__)
 
 
-@app.route('/')
-def
-
 if __name__ == '__main__':
     host = getenv('HBNB_API_HOST', default='0.0.0.0')
     port = getenv('HBNB_API_PORT', default=5000)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,4 +3,15 @@
 from api.v1.views import app_views
 
 
-@app_views.route('views/stats')
+@app_views.route('views/stats', methods=['GET'], strict_slashes=False)
+def get_stats():
+    """Retrieves the number of objects by type"""
+    objects_count_dict = {
+        "amenities": 47,
+        "cities": 36,
+        "places": 154,
+        "reviews": 718,
+        "states": 27,
+        "users": 31
+    }
+    return objects_count_dict
