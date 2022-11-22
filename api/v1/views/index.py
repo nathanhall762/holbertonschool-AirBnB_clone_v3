4bee5807e9338d1ee8fa03bc7fed54e2767d28bb
nathanhall762 <4943@holbertonstudents.com>
2022-11-22 13:53:27 -0600

    added app views to views init

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@ from flask import jsonify
 from models import storage
 from api.v1.views import app_views
 
+
 @app_views.route('views/status', methods=['GET'], strict_slashes=False)
 def get_status():
     """Retrieves OK message"""
