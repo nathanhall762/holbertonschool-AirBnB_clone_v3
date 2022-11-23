20807d78a8696a50c4de79af7202e5b552f56094
nathanhall762 <4943@holbertonstudents.com>
2022-11-23 14:24:57 -0600

    add cities delete

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -21,21 +21,25 @@ def all_cities():
 def get_city(city_id):
     """Retrieves a city"""
     # if:  # id is linked to a city object
-    data = storage.get("City", city_id)
+    cities = storage.get("City", city_id)
     #    return jsonify({})  # return city object of city_id
-    if data:
-        return jsonify((data.to_dict(), 200))
+    if cities:
+        return jsonify((cities.to_dict(), 200))
     abort(404)  # a 404 error
 
 
 @app_views.route('/api/v1/cities/<city_id>',
                  methods=['DELETE'], strict_slashes=False)
-def delete_city():
+def delete_city(city_id):
     """Deletes a city"""
+    city = storage.get("City", city_id)
     #  if:  # id is linked to a city object
-    #    return jsonify({})  # return all city objects
-    #  if:  # id is linked to an empty city object
+    if city:
+        city.delete()
+        storage.save()
     #    return  # empty dictionary
+        return (jsonify({}), 200)
+    #  if:  # id is linked to an empty city object
     abort(404)  # a 404 error
 
 
