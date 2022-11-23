abb425e6375333228c1984f55110ee97b6a9e7ac
nathanhall762 <4943@holbertonstudents.com>
2022-11-23 16:02:49 -0600

    added cors maybe

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,9 +4,11 @@ from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from os import environ
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app, origins='0.0.0.0')
 app.register_blueprint(app_views)
 
 
