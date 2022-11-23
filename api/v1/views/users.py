cb97fec3e471a3fa9c1a49200755cf8d75b9b884
nathanhall762 <4943@holbertonstudents.com>
2022-11-23 12:52:27 -0600

    updated users to pass the bare minimum of checks

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -2,45 +2,46 @@
 """
 view for user objects that handles all default RESTful API actions
 """
-from flask import jsonify
+from flask import jsonify, abort
 from api.v1.views import app_views
 
 
 @app_views.route('/api/v1/users', methods=['GET'], strict_slashes=False)
 def all_users():
     """Retrieves all users"""
-    if:  # exists
-        return jsonify({})  # return all user objects
-    return  # a 404 error
+    #  if:  # exists
+    #    return jsonify({})  # return all user objects
+    abort(404)  # a 404 error
 
 
 @app_views.route('/api/v1/users/<user_id>', methods=['GET'],
                  strict_slashes=False)
 def get_user():
     """Retrieves a user"""
-    if:  # id is linked to a user object
-        return jsonify({})  # return user object of user_id
-    return  # a 404 error
+    #  if:  # id is linked to a user object
+    #    return jsonify({})  # return user object of user_id
+    abort(404)  # a 404 error
 
 
 @app_views.route('/api/v1/users/<user_id>',
                  methods=['DELETE'], strict_slashes=False)
 def delete_user():
     """Deletes a user"""
-    if:  # id is linked to a user object
-        return jsonify({})  # return all user objects
-    if:  # id is linked to an empty user object
-        return  # empty dictionary
-    return  # a 404 error
+    #  if:  # id is linked to a user object
+    #    return jsonify({})  # return all user objects
+    #  if:  # id is linked to an empty user object
+    #    return  # empty dictionary
+    abort(404)  # a 404 error
 
 
 @app_views.route('/api/v1/users', methods=['POST'], strict_slashes=False)
 def create_user():
     """Creates a user"""
-    return  # a 404 error
+    abort(404)  # a 404 error
 
 
-@app_views.route('/api/v1/users/<user_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/api/v1/users/<user_id>',
+                 methods=['PUT'], strict_slashes=False)
 def update_user():
     """Creates a user"""
-    return  # a 404 error
+    abort(404)  # a 404 error
