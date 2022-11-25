aa99e7f371e64f4164b9ce6686d7d9d724f57560
nathanhall762 <4943@holbertonstudents.com>
2022-11-25 13:38:57 -0600

    removed " from state and changed state import

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,14 +4,15 @@ view for State objects that handles all default RESTful API actions
 """
 from flask import jsonify, abort, request
 from api.v1.views import app_views
-from models import storage, state
+from models import storage
+from models.state import State
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def all_states():
     """Retrieves all states"""
     state_list = []
-    for state in storage.all("State").values():
+    for state in storage.all(State).values():
         state_list.append(state.to_dict())
     return jsonify(state_list)
 
@@ -20,7 +21,7 @@ def all_states():
                  strict_slashes=False)
 def get_state(state_id=None):
     """Retrieves a state"""
-    state = storage.get("State", state_id)
+    state = storage.get(State, state_id)
     if state is None:
         abort(404)  # a 404 error
     else:
@@ -31,7 +32,7 @@ def get_state(state_id=None):
                  methods=['DELETE'], strict_slashes=False)
 def delete_state(state_id=None):
     """Deletes a state"""
-    state = storage.get("City", state_id)
+    state = storage.get(State, state_id)
     if state is None:
         abort(404)  # a 404 error
     else:
@@ -49,7 +50,7 @@ def create_state():
     elif 'name' not in request.keys():
         abort(400, "Missing name")
     else:
-        new_state = state.State(**state)
+        new_state = State(**request)
         storage.new(new_state)
         storage.save()
         return jsonify(new_state.to_dict()), 201
@@ -58,7 +59,7 @@ def create_state():
 @app_views.route('/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id=None):
     """Creates a state"""
-    state = storage.get("State", state_id)
+    state = storage.get(State, state_id)
     if state is None:
         abort(404)
     update = request.get_json(silent=True)
