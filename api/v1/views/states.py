454a04ee958b569c556aecda7ff4b2c766f1fedd
nathanhall762 <4943@holbertonstudents.com>
2022-11-28 13:24:34 -0600

    imported make_response in states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,7 +2,7 @@
 """
 view for State objects that handles all default RESTful API actions
 """
-from flask import jsonify, abort, request
+from flask import jsonify, abort, request, make_response
 from api.v1.views import app_views
 from models import storage
 from models.state import State
